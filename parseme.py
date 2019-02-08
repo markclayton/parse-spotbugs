29e3aee354a0a934dcdd86b9fc236225d0d33e3b
Mark Brandon Clayton (TMNA) <mark.b.clayton@toyota.com>
2019-02-08 16:00:10 -0600

    had to adjust severity guage

--- parseme.py
+++ parseme.py
@@ -20,13 +20,19 @@ with open('target/spotbugsXml.xml') as fd2:
 
 output['project_name'] = str(spotbugs_doc['BugCollection']['Project']['@projectName'])
 
+# https://spotbugs.readthedocs.io/en/stable/filter.html
+# <Rank>
+# This element matches warnings with a particular bug rank. 
+# The value attribute should be an integer value between 1 and 20, 
+# where 1 to 4 are scariest, 5 to 9 scary, 10 to 14 troubling, and 15 to 20 of concern bugs.
+
 for bug in spotbugs_doc['BugCollection']['BugInstance']:
-    priority = str(bug['@priority'])
-    if priority == "1":
+    priority = int(bug['@rank'])
+    if priority in range(15,20):
         low += 1 
-    elif priority == "2":
+    elif priority in range(10,14):
         med += 1 
-    elif priority == "3":
+    elif priority in range(1,9):
         high += 1
     bugCount += 1 
 
@@ -35,6 +41,6 @@ output['spotbugs_low'] = low
 output['spotbugs_med'] = med
 output['spotbugs_high'] = high
 
-r = requests.post(endpoint, data = output)
+# r = requests.post(endpoint, data = output)
 print(output)
 
